097e699267352a8a104c27076c6f7e25dff3975a
adeleyeayodeji <biodunhi@gmail.com>
2024-10-16 13:09:02 +0100

    first commit

---
+++ tower-of-hanoi-api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Tower Of Hanoi Api
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     tower-of-hanoi-api
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Tower_Of_Hanoi_Api
+ */
+
+// Your code starts here.
